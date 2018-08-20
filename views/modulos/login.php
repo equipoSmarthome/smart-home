3c16e75fe93edb8eb251397063edcbda4d7b48b6
ErwinZamorata <34925825+ErwinZamorata@users.noreply.github.com>
2018-08-20 02:09:32 -0300

    Login y Salir
    Se agrega parametro name para comparar datos con la tabla usuarios (user y password) se utiliza Session y se cierra la sesion del usuario por medio del boton "cerrar.

---
+++ views/modulos/login.php
@@ -0,0 +1,106 @@
+<?php 
+require_once "../../controllers/sesion.controller.php";
+require_once "../../models/sesion.modelo.php";
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/fontawesome.min.css">
+    <link rel="stylesheet" href="../css/estilos.css">
+
+    <title>Login</title>
+
+</head>
+
+<body class="fondo">
+    <div class="btn btn-primary" id="volverInicio">Volver</div>
+    <div class="opacidad"></div>
+
+    <div class="container mt-5">
+
+        <div class="row justify-content-center">
+            <!-- inicio del logo -->
+            <div class="col-12">
+                <img class="img-fluid" src="../img/logo/logo.png" alt="">
+            </div>
+                <!-- fin del logo -->
+
+                <!-- inicio del formulario -->
+            <div class="col-md-5">
+                <form method="post">
+                    <div class="input-group mb-5 mt-2">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-user"></i></span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Correo" aria-label="Username"name="user" required>
+                    </div>
+
+                    <div class="input-group mb-5">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-key"></i></span>
+                        </div>
+                        <input type="password" class="form-control" placeholder="Contraseña" aria-label="Username" name="password" required>
+                    </div>
+
+                    <div class="row justify-content-center">
+                        <div class="col-10">
+                            <button type="submit" class="btn btn-primary btn-block">Iniciar Sesion</button>
+                        </div>
+                    </div>
+                          <?php 
+                              $iniciarSesion = new ControllerSesion();
+                              $iniciarSesion -> IniciarSesionCtr();
+                         ?>
+                </form>
+            </div>
+                <!-- fin del formulario -->
+
+                <!-- link de recuperar contraseña -->
+
+            <div class="col-12 mt-4">
+                <a class="link-recupera" href="#" data-toggle="modal" data-target="#modal-contraseña">Recuperar Contraseña</a>
+            </div>
+            <!-- fin del recuperar contraseña -->
+
+            <!-- inicio de ventana modal -->
+            <div class="modal fade" id="modal-contraseña" tabindex="-1" role="dialog" aria-labelledby="#modal-contraseña" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Recuperar Contraseña</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p class="">Para Recuperar su Contraseña, Ingrese su Correo Electronico</p>
+                            <div class="col-auto input-group mb-3 mt-1">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-default"><i class="fas fa-user"></i></span>
+                                </div>
+                                <input type="email" class="form-control" placeholder="Ingrese su Correo">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary">Enviar</button>
+                            <button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- fin ventana modal -->
+        </div>
+
+
+    </div>
+    <script src="../js/jquery-3.3.1.min.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/fontawesome.min.js"></script>
+    <script src="../js/acciones.js"></script>
+</body>
+</html>
