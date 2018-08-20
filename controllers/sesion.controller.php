f4d7fd7170198d78688fbd16a593fd1a1c50d2a0
ErwinZamorata <eduardo.zamorata@gmail.com>
2018-08-20 01:22:33 -0300

    Se agrega el controlador para el inicio de sesión del usuario

---
+++ controllers/sesion.controller.php
@@ -0,0 +1,28 @@
+<?php 
+class ControllerSesion {
+	public function IniciarSesionCtr(){
+		if (isset($_POST["user"])) {
+			$tabla = "usuarios";
+			$usuario = $_POST["user"];
+			$respuesta = ModeloSesion::iniciarSesionMdl($tabla, $usuario);
+			if ($respuesta["correo"] == $_POST["user"] && $respuesta["password"] == $_POST["password"] ){
+				$_SESSION["autenticar"] = "ok";
+				$_SESSION["nombre"] = $respuesta["correo"];
+				$_SESSION["id"] = $respuesta["id"];
+				echo '
+					<script>
+						window.location = "../menu.php"
+					</script>
+
+				';	
+			} else {
+				echo '
+					<div class="alert alert-danger">
+					Datos Incorrectos. Intentelos nuevamente</div>
+				';
+			}
+		}
+	}
+}
+
+?>
